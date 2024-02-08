b5917f7f072f724736add55fd114a33d9de4945a
nakajimakouji <f.f-super1142@au.com>
2024-02-08 23:46:37 +0900

    3.モデルスペック

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,47 +1,69 @@
 require 'rails_helper'
 
 RSpec.describe Project, type: :model do
+  it "is valid with a name" do
+    user = User.create(
+      first_name: "Joe",
+      last_name: "Tester",
+      email: "joetester@example.com",
+      password: "dottle-nouveau-pavilion-furze"
+    )
+
+    project = Project.new(
+      name: "Test Project",
+      user_id: user.id
+    )
+    expect(project).to be_valid
+  end
+
+  it "is invalid without a name" do
+    project = Project.new(name: nil)
+    project.valid?
+    expect(project.errors[:name]).to include("can't be blank")
+  end
+
+  # ユーザー単位では重複したプロジェクト名を許可しないこと
   it "does not allow duplicate project names per user" do
     user = User.create(
       first_name: "Joe",
-      last_name:  "Tester",
-      email:      "joetester@example.com",
-      password:   "dottle-nouveau-pavilion-tights-furze",
+      last_name: "Tester",
+      email: "joetester@example.com",
+      password: "dottle-nouveau-pavilion-furze"
     )
 
     user.projects.create(
-      name: "Test Project",
+      name: "Test Project"
     )
 
     new_project = user.projects.build(
-      name: "Test Project",
+      name: "Test Project"
     )
-
     new_project.valid?
     expect(new_project.errors[:name]).to include("has already been taken")
   end
 
-  it "allows two users to share a project name" do
+  # 二人のユーザーが同じ名前を使うことは許可すること
+  it "allow two users to share a project name" do
     user = User.create(
       first_name: "Joe",
-      last_name:  "Tester",
-      email:      "joetester@example.com",
-      password:   "dottle-nouveau-pavilion-tights-furze",
+      last_name: "Tester",
+      email: "joetester@example.com",
+      password: "dottle-nouveau-pavilion-furze"
     )
 
     user.projects.create(
-      name: "Test Project",
+      name: "Test Project"
     )
 
     other_user = User.create(
       first_name: "Jane",
-      last_name:  "Tester",
-      email:      "janetester@example.com",
-      password:   "dottle-nouveau-pavilion-tights-furze",
+      last_name: "Tester",
+      email: "janetester@example.com",
+      password: "dottle-nouveau-pavilion-furze"
     )
 
     other_project = other_user.projects.build(
-      name: "Test Project",
+      name: "Test Project"
     )
 
     expect(other_project).to be_valid
